1495d42d4f50b3b168bd29ee4925db9e2afe444a
Ben Manekin <ben.manekin@gmail.com>
2019-02-12 22:25:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,68 @@
+require 'pry'
 # Write your code here.
+def dictionary
+  sub_hash = {
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"}
+end
+
+
+def word_substituter(tweet)
+    tweet.split(" ").map do |word|
+      if dictionary.keys.include?(word.downcase)
+        word = dictionary[word.downcase]
+      else
+        word
+    end
+  end.join(" ")
+end
+
+
+def bulk_tweet_shortener(array_tweets)
+  array_tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet = nil
+    puts "Try again"
+  elsif tweet.to_s.length < 140
+    puts tweet
+  else
+    puts word_substituter(tweet)
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if tweet = nil
+    puts "Try again"
+  elsif tweet.length < 140
+    puts tweet
+  else
+    truncd = word_substituter(tweet)
+    if truncd.length > 140
+      final = truncd[0..134] << "(...)"
+      final
+    else
+      truncd
+    end
+  end
+end
+
+
+  
+  
+  
+  
+  
+  
+  
